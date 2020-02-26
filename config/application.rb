ca21d43fbc91020b984d00c449ac670a3e2be2c4
Kurnia Muhamad <omenkzz@gmail.com>
2020-02-27 00:34:55 +0700

    Remove assets and helper generator

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,10 @@ module Swanten
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.helper false
+      g.assets false
+    end
+    
   end
 end
